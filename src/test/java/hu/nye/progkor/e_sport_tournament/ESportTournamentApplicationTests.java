7d1bd62c0f4c693f9f7445ae4ff669bb8fcbc969
ITiboht <itiboht@gmail.com>
2022-05-12 14:23:05 +0200

    Initial commit

---
+++ src/test/java/hu/nye/progkor/e_sport_tournament/ESportTournamentApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.nye.progkor.e_sport_tournament;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ESportTournamentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
